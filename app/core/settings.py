605a36437638de353505402193ea31fa658dee9b
Abel Alazar Dirar <aadlinux358@gmail.com>
2023-05-05 02:07:36 +0300

    add: fastapi_jwt_auth asymmetric algorithm and update settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,11 +1,33 @@
 """Human resources application settings module."""
+import os
+import sys
 from typing import Union
 
 from pydantic import BaseSettings, PostgresDsn
+import requests
+from requests.exceptions import ConnectionError
+from dotenv import load_dotenv
 
+load_dotenv()
+
+def get_public_key() -> str:
+    auth_api = os.environ.get('auth_api')
+    if auth_api is None:
+        print('auth_api environment variable not set.')
+        sys.exit(1)
+    try:
+        response = requests.get(f"{auth_api}/api/v1/auth/public-key")
+    except ConnectionError as e:
+        print('Can not obtain public key. Check Auth service.')
+        sys.exit(1)
+
+    return response.json()['public_key']
 
 class Settings(BaseSettings):
     """Settings configuration class."""
+    # fastapi_jwt_auth
+    authjwt_public_key: str = get_public_key()
+    authjwt_algorithm: str = 'RS256'
 
     # Base
     api_v1_prefix: str
