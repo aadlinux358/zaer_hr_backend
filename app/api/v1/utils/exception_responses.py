46161d26368c9016300273427f5e87d04bd11810
Abel Alazar Dirar <aadlinux358@gmail.com>
2023-05-14 18:12:57 +0300

    add: staff_user_or_error util function.

--- app/api/v1/utils/exception_responses.py
+++ app/api/v1/utils/exception_responses.py
@@ -15,3 +15,16 @@ async def superuser_or_error(user_claims: Optional[dict]) -> None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="insufficient privileges."
         )
+
+
+async def staff_user_or_error(user_claims: Optional[dict]) -> None:
+    """Check if user is staff member."""
+    if user_claims is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="invalid token claim."
+        )
+
+    if not user_claims.get("is_staff"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="insufficient privileges."
+        )
